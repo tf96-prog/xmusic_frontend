e638affb45fa42ced888dacbe512d6a6fdcd56d1
tomas <supertomy77@hotmail.com>
2025-02-20 15:53:40 -0300

    Adding Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
 import Canciones from "./components/Canciones";
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { AppShell, Burger, Group, Skeleton, MantineProvider } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 function App() {
+
+  const [opened, { toggle }] = useDisclosure();
   
   return (
     <>
-      <MantineProvider><Canciones /></MantineProvider>
+      <MantineProvider>
+        <AppShell
+      header={{ height: 60 }}
+      footer={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      aside={{ width: 300, breakpoint: 'md', collapsed: { desktop: false, mobile: true } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Canciones/>
+      </AppShell.Main>
+      <AppShell.Aside p="md">Aside</AppShell.Aside>
+      <AppShell.Footer p="md">Footer</AppShell.Footer>
+    </AppShell>
+    </MantineProvider>
     </>
     
   )
 }
 
 export default App
+
+
+
+
+
